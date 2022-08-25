ff4de53d863e9b0436f22468839dfef56ae1c5be
IsaaMaster <songisaac17@gmail.com>
2022-08-24 17:37:13 -0700

    init

---
+++ login/loginbasic/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+urlpatterns = [
+    path("", views.index, name="home"), 
+    path("notes/", views.notes, name="notes"),
+    path("notes/<int:note_id>/", views.detail, name="detail"),
+    path("notes/new", views.createNewNote, name="new"),  
+    path("login", views.loginpage, name="loginpage"),
+    path("userlogintrue", views.user_login, name="userlogin"), 
+    path("register", views.user_registration, name='register'),
+
+
+]
